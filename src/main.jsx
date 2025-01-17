60b2df05b3a7582be9565e8d0aadbd1a2e5db4d5
somin2352 <somin030502@gmail.com>
2025-01-17 14:50:41 +0900

    vite+react 환경 구성 및 css modules 추가

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <h1 className="hello">안녕</h1>
+    </StrictMode>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+const viteConfig = defineConfig({
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  preview: {
+    host: "localhost",
+    port: 8080,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
+export default viteConfig;
