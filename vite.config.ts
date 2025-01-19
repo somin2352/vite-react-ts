a9b1ceb3fea82d32c1ebbae4276d09df94c96a03
somin2352 <somin030502@gmail.com>
2025-01-19 14:45:03 +0900

    [과제2] 커스텀 앱 빌드 환경 구성
    - ESLint 린팅 구성
    - Prettier 포멧팅 구성
    - 절대 경로 구성
    - Hursky
    - lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 
 const viteConfig = defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic",
+      jsxRuntime: 'automatic',
     }),
   ],
   server: {
-    host: "localhost",
+    host: 'localhost',
     port: 3000,
   },
   preview: {
-    host: "localhost",
+    host: 'localhost',
     port: 8080,
   },
   css: {
     devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
 export default viteConfig;
